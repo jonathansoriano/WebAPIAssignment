3116507e7fe6dedad698d474237668876519d388
Jon Sanjuan <sorianjn@mail.uc.edu>
2024-10-30 20:10:14 -0400

    Created a new Controller

---
+++ WebAPIAssignment/Controllers/JonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAssignment.Models;
+
+namespace WebAPIAssignment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JonController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Jon jon = new Jon("Jon Sanjuan");
+            return Ok(jon);
+        }
+    }
+}
---
+++ WebAPIAssignment/Models/Jon.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPIAssignment.Models
+{
+    public class Jon
+    {
+        public string Name { get; set; }
+
+        public Jon() { }
+
+        public Jon(string name) 
+        { 
+            Name = name;
+        }
+
+    }
+}
